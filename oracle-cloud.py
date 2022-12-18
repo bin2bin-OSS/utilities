45c60f9c0dd85b15c8d8e85e9a361291c3696751
Mohankumar <mohanram@bin2bin.in>
2022-12-18 11:02:42 +0530

    backup

--- oracle-cloud.py
+++ oracle-cloud.py
@@ -29,7 +29,6 @@ users:
       - {ssh_public_key}
 
 runcmd:
-  - mkdir -p /root/install_logs
   - echo "Running run commands from cloud init user data ..."
   - echo "PermitRootLogin prohibit-password" >> /etc/ssh/sshd_config
   - systemctl restart ssh
